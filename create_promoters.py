72ef63f3b0f82d55db04f6a3bf006d9cd65ee734
Hrant Hovhannisyan <grant.hovhannisyan@gmail.com>
2019-06-19 13:53:59 +0200

    Update create_promoters.py

--- create_promoters.py
+++ create_promoters.py
@@ -25,7 +25,7 @@ with open(sys.argv[1], "r+") as gff_file:
 				gene_name=line[8].split(";")[0][9:-1]
 				#print gene_name
 				gff_dict[line[0]+"*"+gene_name+"*"+line[6]]=coords
-	print len(gff_dict)			
+	#print len(gff_dict)			
 
 	#~ for k,v in gff_dict.iteritems():
 		#~ print k,v
@@ -64,6 +64,7 @@ with open(sys.argv[2], "r") as genome, open(sys.argv[3], "w") as output:
 								#print sequence, gene.split("*")[1], int(coords[0])-500, "this on +, normal"
 								if len(sequence)>100:
 									output.write(">%s=%s=%s=%s=%s=%s=%s\n%s\n"%(record.id,gene.split("*")[1],int(max_coord)+1,int(coords[0])-1,len(sequence),gene.split("*")[2],"OVERLAPPED",sequence))
+									print("%s\t%s\t%s\t%s\t%s"%(record.id,gene.split("*")[1],int(max_coord)+1,int(coords[0])-1,len(sequence)))
 								else:
 									n_short_genes+=1
 							elif overlap_counter==0:
@@ -72,6 +73,7 @@ with open(sys.argv[2], "r") as genome, open(sys.argv[3], "w") as output:
 								#print sequence, gene.split("*")[1], int(coords[0])-500, "this on +, normal"
 								if len(sequence)>100:	
 									output.write(">%s=%s=%s=%s=%s=%s\n%s\n"%(record.id,gene.split("*")[1],coords[0],coords[1],len(sequence),gene.split("*")[2],sequence))
+									print("%s\t%s\t%s\t%s\t%s"%(record.id,gene.split("*")[1],coords[0],coords[1],len(sequence)))
 								else:
 									n_short_genes=+1
 						elif int(coords[0])>50 and int(coords[0])<int(prom_len):
@@ -98,6 +100,7 @@ with open(sys.argv[2], "r") as genome, open(sys.argv[3], "w") as output:
 								#print sequence, gene.split("*")[1], int(coords[0])-500, "this on +, normal"
 								if len(sequence)>100:	
 									output.write(">%s=%s=%s=%s=%s=%s=%s\n%s\n"%(record.id,gene.split("*")[1],int(max_coord)+1,int(coords[0])-1,len(sequence),gene.split("*")[2],"OVERLAPPED_overflow",sequence))
+									print("%s\t%s\t%s\t%s\t%s"%(record.id,gene.split("*")[1],int(max_coord)+1,int(coords[0])-1,len(sequence)))
 								else:
 									n_genes_short=+1
 							elif overlap_counter==0:
@@ -106,6 +109,7 @@ with open(sys.argv[2], "r") as genome, open(sys.argv[3], "w") as output:
 								#print len(sequence), gene.split("*")[1], int(coords[0])-500, "this on +, short"
 								if len(sequence)>100:
 									output.write(">%s=%s=%s=%s=%s=%s=%s\n%s\n"%(record.id,gene.split("*")[1],coords[0],coords[1],len(sequence),gene.split("*")[2],"overflow",sequence))
+									print("%s\t%s\t%s\t%s\t%s"%(record.id,gene.split("*")[1],coords[0],coords[1],len(sequence)))
 								else:
 									n_short_genes+=1
 					elif gene.split("*")[2]=="-":
@@ -135,6 +139,7 @@ with open(sys.argv[2], "r") as genome, open(sys.argv[3], "w") as output:
 								#print sequence, gene.split("*")[1], int(coords[0])-500, "this on +, normal"
 								if len(sequence)>100:
 									output.write(">%s=%s=%s=%s=%s=%s=%s\n%s\n"%(record.id,gene.split("*")[1],int(coords[1]),int(min_coord),len(sequence),gene.split("*")[2],"OVERLAPPED",reverse_seq))
+									print("%s\t%s\t%s\t%s\t%s"%(record.id,gene.split("*")[1],int(coords[1]),int(min_coord),len(sequence)))
 								else:
 									n_short_genes+=1
 							elif overlap_counter_m==0:
@@ -145,6 +150,7 @@ with open(sys.argv[2], "r") as genome, open(sys.argv[3], "w") as output:
 								#print reverse_seq, gene.split("*")[1],"this is on -, normal"
 								if len(sequence)>100:
 									output.write(">%s=%s=%s=%s=%s=%s\n%s\n"%(record.id,gene.split("*")[1],int(coords[1]),int(coords[1])+int(prom_len),len(reverse_seq),gene.split("*")[2],reverse_seq))
+									print("%s\t%s\t%s\t%s\t%s"%(record.id,gene.split("*")[1],int(coords[1]),int(coords[1])+int(prom_len),len(reverse_seq)))
 								else:
 									n_short_genes+=1
 						elif int(coords[1])+int(prom_len)>len(record.seq):
@@ -172,6 +178,7 @@ with open(sys.argv[2], "r") as genome, open(sys.argv[3], "w") as output:
 								#print sequence, gene.split("*")[1], int(coords[0])-500, "this on +, normal"
 								if len(sequence)>100:	
 									output.write(">%s=%s=%s=%s=%s=%s=%s\n%s\n"%(record.id,gene.split("*")[1],int(coords[1]),int(min_coord),len(sequence),gene.split("*")[2],"OVERLAPPED_overflow",reverse_seq))
+									print("%s\t%s\t%s\t%s\t%s"%(record.id,gene.split("*")[1],int(coords[1]),int(min_coord),len(sequence)))
 								else:
 									n_short_genes+=1
 							elif overlap_counter_m==0:
@@ -181,9 +188,15 @@ with open(sys.argv[2], "r") as genome, open(sys.argv[3], "w") as output:
 								#print reverse_seq, gene.split("*")[1], "this is on -, short"
 								if len(reverse_seq)>100:
 									output.write(">%s=%s=%s=%s=%s=%s=%s\n%s\n"%(record.id,gene.split("*")[1],coords[1],len(record.seq),len(reverse_seq),gene.split("*")[2],"overflow",reverse_seq))
+									print("%s\t%s\t%s\t%s\t%s"%(record.id,gene.split("*")[1],coords[1],len(record.seq),len(reverse_seq)))
 								else:
 									n_short_genes+=1
 			else:
 				continue
-print n_short_genes
-print counter
+#print n_short_genes
+#print counter
+
+	
+	
+	
+
